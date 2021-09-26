5c43af81738cd4740f8d53f7bfe10eec2829c012
kraxarn <kraxie@protonmail.com>
2021-09-26 21:45:05 +0200

    Use shorthand instead

--- src/time.rs
+++ src/time.rs
@@ -2,6 +2,6 @@
 pub fn current_timestamp() -> u64 {
 	std::time::SystemTime::now()
 		.duration_since(std::time::UNIX_EPOCH)
-		.unwrap_or(Default::default())
+		.unwrap_or_default()
 		.as_secs()
 }
