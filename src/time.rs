aa8177e4afcf934635e0c0a2e25bea6a0ae43005
kraxarn <kraxie@protonmail.com>
2021-11-07 19:16:30 +0100

    Remove Rust code

--- src/time.rs
+++
@@ -1,7 +0,0 @@
-/// Seconds since the beginning of time (1970-01-01)
-pub fn current_timestamp() -> u64 {
-	std::time::SystemTime::now()
-		.duration_since(std::time::UNIX_EPOCH)
-		.unwrap_or_default()
-		.as_secs()
-}
