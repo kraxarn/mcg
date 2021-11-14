3667594ee7144f0ec80b828409f2119db86cf8d2
kraxarn <kraxie@protonmail.com>
2021-11-14 12:32:57 +0100

    Playing card comparison

--- Shared/Entities/PlayingCard.cs
+++ Shared/Entities/PlayingCard.cs
@@ -1,9 +1,10 @@
+using System;
 using MobileCardGames.Shared.Enums;
 using MobileCardGames.Shared.Extensions;
 
 namespace MobileCardGames.Shared.Entities
 {
-	public class PlayingCard
+	public class PlayingCard : IEquatable<PlayingCard>
 	{
 		public PlayingCard(PlayingCardValue value, PlayingCardSuit suit)
 		{
@@ -17,5 +18,25 @@ namespace MobileCardGames.Shared.Entities
 
 		public override string ToString() =>
 			$"{Value.GetName()} of {Suit.GetName()}";
+
+		public bool Equals(PlayingCard other)
+		{
+			return Value == other.Value
+				&& Suit == other.Suit;
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return obj is PlayingCard playingCard
+				&& Equals(playingCard);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return ((int)Value * 397) ^ (int)Suit;
+			}
+		}
 	}
 }
