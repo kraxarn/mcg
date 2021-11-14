4f971ccea6763598c1dfd598514839236340e946
kraxarn <kraxie@protonmail.com>
2021-11-14 13:29:12 +0100

    Logic for drawing cards from deck

--- Source/Scenes/Dev/Deck.cs
+++ Source/Scenes/Dev/Deck.cs
@@ -1,13 +1,64 @@
+using Godot;
 using MobileCardGames.Constants;
+using MobileCardGames.Extensions;
 using MobileCardGames.Scenes;
 using MobileCardGames.Shared.Enums;
 
 public class Deck : SceneBase
 {
+	private MobileCardGames.Shared.Entities.Deck? deck;
+
+	private Button? drawCardButton;
+
+	private TextureRect? cardTexture;
+	private Label? cardLabel;
+
 	public override void _Ready()
 	{
 		Connect("BackButton", SignalConstants.Pressed, nameof(GoBack));
+
+		drawCardButton = GetNode<Button>("DrawCard");
+		drawCardButton.Connect(SignalConstants.Pressed, this, nameof(DrawCard));
+
+		cardTexture = GetNode<TextureRect>("Container/CardTexture");
+		cardLabel = GetNode<Label>("Container/CardName");
+
+		deck = new MobileCardGames.Shared.Entities.Deck();
+		deck.Shuffle();
+
+		DrawCard();
 	}
 
 	private void GoBack() => GoTo(Scene.DevMenu);
+
+	private void DrawCard()
+	{
+		var card = deck?.Draw();
+		if (card == null)
+		{
+			if (drawCardButton != null)
+			{
+				drawCardButton.Text = "Deck empty";
+			}
+
+			return;
+		}
+
+		if (cardLabel != null)
+		{
+			cardLabel.Text = card.ToString();
+		}
+
+		if (cardTexture?.Texture is AtlasTexture cardAtlas)
+		{
+			var region = cardAtlas.Region;
+			region.Position = card.GetAtlasPosition();
+			cardAtlas.Region = region;
+		}
+
+		if (drawCardButton != null)
+		{
+			drawCardButton.Text = $"Draw card {MobileCardGames.Shared.Entities.Deck.Size - deck?.Count + 1}";
+		}
+	}
 }
