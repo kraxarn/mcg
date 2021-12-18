86f0c1a35e26abc95ecb33decf00b7b4f254a1ac
kraxarn <kraxie@protonmail.com>
2021-12-18 12:08:00 +0100

    Temporarily remove Android support

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -1,9 +1,6 @@
 #[cfg(any(target_os = "linux", target_os = "macos", windows))]
 mod desktop;
 
-#[cfg(target_os = "android")]
-mod android;
-
 #[cfg(target_arch = "wasm32")]
 mod wasm;
 
@@ -12,10 +9,6 @@ pub fn write(app_name: &str, name: &str, data: &str) -> std::io::Result<()> {
 	#[cfg(any(target_os = "linux", target_os = "macos", windows))]
 	{
 		return desktop::write(app_name, name, data);
-	}
-	#[cfg(target_os = "android")]
-	{
-		return android::write(name, data);
 	}
 	#[cfg(target_arch = "wasm32")]
 	{
@@ -28,10 +21,6 @@ pub fn read(app_name: &str, name: &str) -> std::io::Result<String> {
 	#[cfg(any(target_os = "linux", target_os = "macos", windows))]
 	{
 		return desktop::read(app_name, name);
-	}
-	#[cfg(target_os = "android")]
-	{
-		return android::read(name);
 	}
 	#[cfg(target_arch = "wasm32")]
 	{
