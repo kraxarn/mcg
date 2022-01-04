6d3731a99b5c3c5078819f121f215490306cb958
kraxarn <kraxie@protonmail.com>
2022-01-04 17:22:55 +0100

    Redirect to /login if not logged in

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,10 +1,9 @@
 import cookie from "cookie"
-import { v4 as uuid } from "@lukeed/uuid"
 import type { Handle } from "@sveltejs/kit"
 
 export const handle: Handle = async ({ request, resolve }) => {
 	const cookies = cookie.parse(request.headers.cookie || "")
-	request.locals.userid = cookies.userid || uuid()
+	request.locals.userid = cookies.userid
 
 	// TODO https://github.com/sveltejs/kit/issues/1046
 	const method = request.url.searchParams.get("_method")
@@ -12,20 +11,15 @@ export const handle: Handle = async ({ request, resolve }) => {
 		request.method = method.toUpperCase()
 	}
 
-	const response = await resolve(request)
-
-	if (!cookies.userid) {
-		// if this is the first time the user has visited this app,
-		// set a cookie so that we recognise them when they return
-		response.headers["set-cookie"] = cookie.serialize(
-			"userid",
-			request.locals.userid,
-			{
-				path: "/",
-				httpOnly: true,
+	if (!cookies.userid
+		&& !request.url.pathname.endsWith("/login")) {
+		return {
+			status: 302,
+			headers: {
+				Location: "/login",
 			},
-		)
+		}
 	}
 
-	return response
+	return resolve(request)
 }
