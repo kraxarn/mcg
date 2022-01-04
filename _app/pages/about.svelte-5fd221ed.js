7db62dfa5174113829d7efd0778bde3d8abe232a
kraxarn <kraxarn@users.noreply.github.com>
2022-01-04 16:23:38 +0000

    deploy: 6d3731a99b5c3c5078819f121f215490306cb958

---
+++ _app/pages/about.svelte-5fd221ed.js
@@ -0,0 +1,13 @@
+import{S as F,i as G,s as N,j as _,e as i,t as o,P as Q,d as a,l as m,c as r,a as d,g as s,b as B,f as V,H as t,I}from"../chunks/vendor-df164758.js";const U=!0,W=!1;function X(z){let h,e,f,b,E,p,S,u,k,x,A,g,P,H,y,J,K,c,O,v,D,j;return{c(){h=_(),e=i("div"),f=i("h1"),b=o("About this app"),E=_(),p=i("p"),S=o("This is a "),u=i("a"),k=o("SvelteKit"),x=o(` app. You can make
+		your own by typing the following into your command line and following the
+		prompts:`),A=_(),g=i("pre"),P=o("npm init svelte@next"),H=_(),y=i("p"),J=o(`The page you're looking at is purely static HTML, with no client-side
+		interactivity needed. Because of that, we don't need to load any
+		JavaScript. Try viewing the page's source, or opening the devtools
+		network panel and reloading.`),K=_(),c=i("p"),O=o("The "),v=i("a"),D=o("TODOs"),j=o(` page illustrates SvelteKit's data loading
+		and form handling. Try using it with JavaScript disabled!`),this.h()},l(l){Q('[data-svelte="svelte-1ine71f"]',document.head).forEach(a),h=m(l),e=r(l,"DIV",{class:!0});var n=d(e);f=r(n,"H1",{});var L=d(f);b=s(L,"About this app"),L.forEach(a),E=m(n),p=r(n,"P",{});var w=d(p);S=s(w,"This is a "),u=r(w,"A",{href:!0});var M=d(u);k=s(M,"SvelteKit"),M.forEach(a),x=s(w,` app. You can make
+		your own by typing the following into your command line and following the
+		prompts:`),w.forEach(a),A=m(n),g=r(n,"PRE",{});var Y=d(g);P=s(Y,"npm init svelte@next"),Y.forEach(a),H=m(n),y=r(n,"P",{});var C=d(y);J=s(C,`The page you're looking at is purely static HTML, with no client-side
+		interactivity needed. Because of that, we don't need to load any
+		JavaScript. Try viewing the page's source, or opening the devtools
+		network panel and reloading.`),C.forEach(a),K=m(n),c=r(n,"P",{});var T=d(c);O=s(T,"The "),v=r(T,"A",{href:!0});var R=d(v);D=s(R,"TODOs"),R.forEach(a),j=s(T,` page illustrates SvelteKit's data loading
+		and form handling. Try using it with JavaScript disabled!`),T.forEach(a),n.forEach(a),this.h()},h(){document.title="About",B(u,"href","https://kit.svelte.dev"),B(v,"href","/todos"),B(e,"class","content svelte-cf77e8")},m(l,q){V(l,h,q),V(l,e,q),t(e,f),t(f,b),t(e,E),t(e,p),t(p,S),t(p,u),t(u,k),t(p,x),t(e,A),t(e,g),t(g,P),t(e,H),t(e,y),t(y,J),t(e,K),t(e,c),t(c,O),t(c,v),t(v,D),t(c,j)},p:I,i:I,o:I,d(l){l&&a(h),l&&a(e)}}}const $=W,tt=U,et=!0;class at extends F{constructor(h){super();G(this,h,null,X,N,{})}}export{at as default,$ as hydrate,et as prerender,tt as router};
