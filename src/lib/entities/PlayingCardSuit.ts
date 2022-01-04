14da3050cd8f18d522dfbfc62404aaea4efde48e
kraxarn <kraxie@protonmail.com>
2022-01-05 00:11:19 +0100

    Remove suit/value entities

--- src/lib/entities/PlayingCardSuit.ts
+++
@@ -1,9 +0,0 @@
-export default class PlayingCardSuit {
-	private suite: string
-
-	private constructor() {}
-
-	private getSuits(): string[] {
-		return ["spades", "clubs", "diamonds", "hearts"]
-	}
-}
