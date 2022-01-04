08b5b94ee76dd5f8d1009df004b9996e25fefa7b
kraxarn <kraxie@protonmail.com>
2022-01-04 17:22:19 +0100

    Initial user storage

---
+++ src/lib/user.ts
@@ -0,0 +1,11 @@
+import { writable } from "svelte/store"
+
+export const user = writable<string>(window.localStorage.getItem("user"))
+
+user.subscribe((value: string) => {
+	window.localStorage.setItem("user", value)
+})
+
+export {
+	user as default,
+}
