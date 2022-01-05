fa5508004b501db13de814d512a5a7aeee5d04bb
kraxarn <kraxie@protonmail.com>
2022-01-05 12:33:49 +0100

    Print version in bottom left

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
 	import "../app.css"
+
+	const version = process.env.npm_package_version
 </script>
 
 <main>
 	<slot />
 </main>
 
+<span id="version">
+	v{version}
+</span>
+
 <style>
 	main {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+
+	#version {
+		position: fixed;
+		font-family: monospace;
+		bottom: 12px;
+		left: 12px;
 	}
 </style>
