94bc420dcf1c513590667af4c47ba498102ec5a9
kraxarn <kraxie@protonmail.com>
2022-01-06 00:38:52 +0100

    New text input component

---
+++ src/lib/components/TextInput.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let id: string
+	export let type: "text" | "password"
+</script>
+
+<div id="input-container">
+	<input {id} {type} />
+</div>
+
+<style>
+	#input-container {
+		display: flex;
+		flex-direction: row;
+		border-image: url("/images/input.webp") 8;
+		border-image-repeat: repeat;
+		border-width: 8px;
+		border-style: solid;
+	}
+
+	input {
+		background-color: #fff1d2;
+		border: 0;
+		flex: 1;
+		padding: 6px;
+	}
+</style>
