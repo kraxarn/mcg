f19cd8970fb7a92c100c7ae91945bcae4c709360
kraxarn <kraxie@protonmail.com>
2022-01-07 13:16:49 +0100

    Initial panel component

---
+++ src/lib/components/Panel.svelte
@@ -0,0 +1,25 @@
+<div class="panel-container">
+	<div class="panel-content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.panel-container {
+		border-image: url("/images/panel.webp") 16;
+		border-image-repeat: repeat;
+		border-width: 8px;
+		border-style: solid;
+	}
+
+	.panel-content {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		gap: 12px;
+		padding: 16px;
+		box-sizing: border-box;
+		flex-direction: column;
+		background-color: #fff1d3;
+	}
+</style>
