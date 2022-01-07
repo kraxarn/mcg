2ee94fdcca8c19ad3487e51e87f464f36832c732
kraxarn <kraxie@protonmail.com>
2022-01-07 13:09:11 +0100

    Initial banner component

---
+++ src/lib/components/Banner.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let title: string
+</script>
+
+<div class="banner-container">
+	<h1>
+		{title}
+	</h1>
+</div>
+
+<style>
+	.banner-container {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		background-image: url("/images/banner.webp");
+		background-size: contain;
+		background-repeat: no-repeat;
+		background-position: center;
+		color: var(--foreground-alt-color);
+		margin-bottom: 16px;
+	}
+
+	h1 {
+		text-align: center;
+	}
+</style>
