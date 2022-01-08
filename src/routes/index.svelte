94b6848e1e231cdfb0c5bca8461ea3fe84fc4abd
kraxarn <kraxie@protonmail.com>
2022-01-08 17:42:21 +0100

    Spinner on start page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,8 +11,21 @@
 	}
 </script>
 
+<script lang="ts">
+	import Spinner from "$lib/components/Spinner.svelte"
+
+	let errorMessage: string
+</script>
+
 <div id="container">
 	<img src="/images/logo.webp" alt="logo" />
+	{#if errorMessage}
+		<span class="error-message">
+			{errorMessage}
+		</span>
+	{:else}
+		<Spinner />
+	{/if}
 </div>
 
 <style>
@@ -20,5 +33,16 @@
 		position: absolute;
 		top: 50%;
 		transform: translate(0, -50%);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 32px;
+		color: var(--foreground-alt-color);
+	}
+
+	.error-message {
+		color: var(--foreground-error-color);
+		font-weight: var(--font-weight-title1);
+		font-size: 1.2em;
 	}
 </style>
