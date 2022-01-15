4406a62a4acf61131dc608626a61794df4981d30
kraxarn <kraxie@protonmail.com>
2022-01-15 16:32:51 +0100

    Icons for buttons

--- src/routes/dev/index.svelte
+++ src/routes/dev/index.svelte
@@ -2,11 +2,15 @@
 	import Button from "$lib/components/Button.svelte"
 	import Banner from "$lib/components/Banner.svelte"
 	import Panel from "$lib/components/Panel.svelte"
+	import {
+		faChevronLeft,
+		faRocket,
+	} from "@fortawesome/free-solid-svg-icons"
 </script>
 
 <Banner title="Developer" />
 
 <Panel>
-	<Button href="/dev/nakama" content="Nakama" />
-	<Button href="/" content="Back" />
+	<Button href="/dev/nakama" content="Nakama" icon="{faRocket}" />
+	<Button href="/" content="Back" icon="{faChevronLeft}" />
 </Panel>
