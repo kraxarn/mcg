5120464a94b5ae3f2ac81e461b8d457cce0d99f2
kraxarn <kraxie@protonmail.com>
2022-12-31 13:51:08 +0100

    Testing crash dialog

--- mcg-desktop/src/main.rs
+++ mcg-desktop/src/main.rs
@@ -1,1 +1,15 @@
-fn main() { mcg::run() }
+use std::panic;
+use native_dialog::{MessageDialog, MessageType};
+
+fn main() {
+	// TODO: Show in main window
+	panic::set_hook(Box::new(move |info| {
+		MessageDialog::new()
+			.set_type(MessageType::Error)
+			.set_title(&format!("{} crashed!", env!("CARGO_PKG_NAME")))
+			.set_text(&format!("{:?}", info))
+			.show_alert()
+			.unwrap();
+	}));
+	mcg::run();
+}
