f33fb69321bf17723c90d24ee0f75cfbee258be6
kraxarn <kraxie@protonmail.com>
2023-01-06 16:11:33 +0100

    Better panic error message

--- mcg-desktop/src/main.rs
+++ mcg-desktop/src/main.rs
@@ -4,12 +4,23 @@ use native_dialog::{MessageDialog, MessageType};
 fn main() {
 	// TODO: Show in main window
 	panic::set_hook(Box::new(move |info| {
+		let mut message = if let Some(payload) = info.payload().downcast_ref::<&str>() {
+			String::from(*payload)
+		} else {
+			String::from("panic")
+		};
+
+		if let Some(location) = info.location() {
+			message.push_str(&format!("\nin {}:{}", location.file(), location.line()));
+		}
+
 		MessageDialog::new()
 			.set_type(MessageType::Error)
 			.set_title(&format!("{} crashed!", env!("CARGO_PKG_NAME")))
-			.set_text(&format!("{:?}", info))
+			.set_text(&message)
 			.show_alert()
 			.unwrap();
 	}));
+
 	mcg::run();
 }
