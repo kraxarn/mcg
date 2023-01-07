8a3e860ff3024031d8ad17a0010a89f5bcb2ba5a
kraxarn <kraxie@protonmail.com>
2023-01-07 13:09:43 +0100

    Fix trying to update non-existent entity

--- src/scenes/dev_card_scene.rs
+++ src/scenes/dev_card_scene.rs
@@ -115,16 +115,16 @@ impl DevCardScene {
 		mut draw_card_event: EventWriter<DrawCardEvent>,
 	) {
 		for event in button_clicked_event.iter() {
-			let children = children.get(event.entity_id).unwrap();
-			let mut text = texts.get_mut(children[0]).unwrap();
-			if let Some(card) = deck.draw() {
-				text.sections[1].value = (Deck::MAX - deck.len()).to_string();
-				draw_card_event.send(DrawCardEvent(card));
-			} else {
-				text.sections[0].value = String::from("Deck empty");
-				text.sections[1].value = String::new();
+			if let Ok(children) = children.get(event.entity_id) {
+				let mut text = texts.get_mut(children[0]).unwrap();
+				if let Some(card) = deck.draw() {
+					text.sections[1].value = (Deck::MAX - deck.len()).to_string();
+					draw_card_event.send(DrawCardEvent(card));
+				} else {
+					text.sections[0].value = String::from("Deck empty");
+					text.sections[1].value = String::new();
+				}
 			}
-			return;
 		}
 	}
 }
